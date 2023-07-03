e9c994ad32dc7aa28e88d70c621c0aa4b1b3ef66
tushar.dutt <tushar.dutt@newvision-software.com>
2023-07-03 18:01:31 +0530

    Final Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Wizard';
+export class AppComponent{
+  title = 'newMat';
+  isLinear = true;
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
+  thirdFormGroup!: FormGroup;
+  fourthFormGroup!: FormGroup;
+  constructor(private _formBuilder: FormBuilder) {}
+  ngOnInit() {
+  this.firstFormGroup = this._formBuilder.group({
+  fname: ['', Validators.required],
+  lname: ['', Validators.required]
+  });
+  this.secondFormGroup = this._formBuilder.group({
+  email: ['', Validators.required],
+  mobile: ['', Validators.required],
+  country:['',Validators.required]
+  });
+  this.thirdFormGroup = this._formBuilder.group({
+    skill1: ['', Validators.required],
+    skill2: ['', Validators.required],
+    skill3:['',Validators.required]
+    });
+    this.fourthFormGroup = this._formBuilder.group({
+      job1: ['', Validators.required],
+      job2: ['', Validators.required],
+      job3:['',Validators.required]
+      });
+  }
+  submit(){
+  console.log(this.firstFormGroup.value);
+  console.log(this.secondFormGroup.value);
+  console.log(this.thirdFormGroup.value);
+  console.log(this.fourthFormGroup.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
